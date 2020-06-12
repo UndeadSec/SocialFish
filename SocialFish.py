ff3213c83d51d4c42bbf5d13704f58ae47ee3312
Porcodio2020 <66803401+Porcodio2020@users.noreply.github.com>
2020-06-12 12:00:27 +0200

    https://instagram.com/veronica.muratori?igshid=c9r9ov4pz05c
    https://instagram.com/veronica.muratori?igshid=c9r9ov4pz05c

--- SocialFish.py
+++ SocialFish.py
@@ -536,7 +536,8 @@ def main():
         app.run(host="0.0.0.0", port=5000)
 
 if __name__ == "__main__":
-    try:
+    try:https://instagram.com/veronica.muratori?igshid=c9r9ov4pz05c
+
         main()
     except KeyboardInterrupt:
         exit(0)
